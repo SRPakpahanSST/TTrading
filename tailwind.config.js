d8b7bd1e17fd8f4910d1fce52d3345a7bb1b588e
SR.Pakpahan,SST <75168433+SRPakpahanSST@users.noreply.github.com>
2026-09-15 15:18:36 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#f59e0b',
+        secondary: '#1e293b',
+      },
+    },
+  },
+  plugins: [],
+}
