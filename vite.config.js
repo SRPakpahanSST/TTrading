870c143d6ef86f51f3849d278c3509e9cfbd8e42
SR.Pakpahan,SST <75168433+SRPakpahanSST@users.noreply.github.com>
2026-09-15 15:17:32 +0700

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.GITHUB_PAGES === 'true'
+    ? '/tenang-trading/'
+    : '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
+})
