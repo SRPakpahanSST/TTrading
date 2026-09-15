41a47743022e37017f4d9e34e37066e6cb32b4e2
SR.Pakpahan,SST <75168433+SRPakpahanSST@users.noreply.github.com>
2026-09-15 16:11:20 +0700

    Create mantraList.js

---
+++ src/data/mantraList.js
@@ -0,0 +1,12 @@
+export const mantraList = [
+  "Sabar menunggu, cepat bertindak.",
+  "Hati tenang, tangan cepat.",
+  "Pasar selalu benar.",
+  "Ambil untungmu, jangan serakah.",
+  "Cut loss, jangan berharap.",
+  "Fokus 1-2 saham. Tenang.",
+  "Sinyal jelas. Berani!",
+  "Masa lalu bukan milikmu.",
+  "Cek Bull-Bear. Masih di atas?",
+  "Hati adalah grafik Candlestick terbesar.",
+]
