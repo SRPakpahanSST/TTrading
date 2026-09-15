6a8adb059f461d192766a33e762b541d91ad9289
SR.Pakpahan,SST <75168433+SRPakpahanSST@users.noreply.github.com>
2026-09-15 15:52:24 +0700

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,29 @@
+export const KONDISI_PASAR = [
+  { value: 'bullish', label: 'Bullish' },
+  { value: 'bearish', label: 'Bearish' },
+  { value: 'sideways', label: 'Sideways' },
+]
+
+export const EMOSI = [
+  { value: 'tenang', label: 'Tenang' },
+  { value: 'cemas', label: 'Cemas' },
+  { value: 'serakah', label: 'Serakah' },
+  { value: 'ragu', label: 'Ragu' },
+]
+
+export const PILAR = [
+  { key: 'pilarRajaMakro', label: 'Raja Makro' },
+  { key: 'pilarPembunuh', label: 'Pembunuh Jangka Pendek' },
+  { key: 'pilarSentuhanEmas', label: 'Sentuhan Emas' },
+  { key: 'pilarDewaFundamental', label: 'Dewa Fundamental' },
+]
+
+export const DOSA = [
+  'Keserakahan',
+  'Ketakutan',
+  'Harapan',
+  'Penyesalan',
+  'Kesombongan',
+  'Keraguan',
+  'Kebingungan',
+]
